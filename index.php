9720a235fa500dfa9d6b1fa8029e785bfccef47c
registration-form <37366733+registration-form@users.noreply.github.com>
2018-03-14 16:17:46 +0600

    Сайт для регистрации

---
+++ index.php
@@ -0,0 +1,41 @@
+<hrml>
+<head>
+<title>Registration-form</title>
+<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+
+<div class="border">
+	<div class="title">TITLE</div>
+	<div class="body">The jQuery Bower package contains additional files besides the default distribution</div>
+</div>
+
+<div class="border">
+	<div class="title">TITLE</div>
+	<div class="body">The jQuery Bower package contains additional files besides the default distribution</div>
+</div>
+
+
+<?php
+$a = "Александр";
+echo "Привет пользователь - " . $a;
+?>
+
+</body>
+
+</html>
+
+
+<style>
+.border{
+	border:1px solid;
+	width:200px;
+	margin:10px;
+}
+
+.title{
+	border-bottom:1px solid black;
+	color:red;
+}
+
+</style>
