a4383e6bc0d3c595548df4bb3e2c2e6201ccbd32
registration-form <37366733+registration-form@users.noreply.github.com>
2018-03-14 16:47:57 +0600

    Hi

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Form for registration</title>
+</head>
+<body>
+
+<h1>Mysite in HTML</h1>
+
+<p>
+<?php
+echo "Hi Saparbek";
+?>
+</p>
+
+<p><a href="">New site Saparbek</a></p>
+
+</body>
+</html>
