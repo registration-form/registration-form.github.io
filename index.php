9cfb48ac1ee0283901cd12555cce8d7b8b8d8d07
registration-form <37366733+registration-form@users.noreply.github.com>
2018-03-14 16:21:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,37 +5,10 @@
 </head>
 <body>
 
-<div class="border">
-	<div class="title">TITLE</div>
-	<div class="body">The jQuery Bower package contains additional files besides the default distribution</div>
-</div>
-
-<div class="border">
-	<div class="title">TITLE</div>
-	<div class="body">The jQuery Bower package contains additional files besides the default distribution</div>
-</div>
-
-
 <?php
-$a = "Александр";
-echo "Привет пользователь - " . $a;
+$a = "Alex";
+echo "New user - ".$a;
 ?>
 
 </body>
-
 </html>
-
-
-<style>
-.border{
-	border:1px solid;
-	width:200px;
-	margin:10px;
-}
-
-.title{
-	border-bottom:1px solid black;
-	color:red;
-}
-
-</style>
